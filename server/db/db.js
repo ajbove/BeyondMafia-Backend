336bc0d57f20ab407204059bfffa943764497eb3
Antony Bove <ajbove@buffalo.edu>
2022-08-16 21:03:53 -0400

    db and routes setup

---
+++ server/db/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "",
+    user: "",
+    password: ""
+  });
+  
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected");
+
+    con.query("CREATE DATABASE mafiaDB", function (err, result) {
+      if (err) throw err;
+      console.log("Database created");
+    });
+  });
