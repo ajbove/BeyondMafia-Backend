9b39e69fdba8f24aafa6ac4c80739cd33c0a7eef
warren772 <wogreen@buffalo.edu>
2022-08-20 00:18:41 -0400

    updates

--- server/db/db.js
+++ server/db/db.js
@@ -1,33 +1,27 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 
 async function initalizationQuery(con){
 var createSchema = `CREATE SCHEMA IF NOT EXISTS mafiaData`
-con.query(createSchema, (err, result) =>{
-  if (err)
-  { throw err;
-  }
-  console.log("Schema Created");
-  var createUserTable = 'CREATE TABLE IF NOT EXISTS `mafiaData`.`userTable` (\
-    `playerid` INT NOT NULL AUTO_INCREMENT,\
-    `username` VARCHAR(45) NOT NULL,\
-    `hash` VARCHAR(45) NOT NULL,\
-    `email` VARCHAR(45) NOT NULL,\
-    `stats` JSON NULL,\
-    `friends` JSON NULL,\
-    `blocked` JSON NULL,\
-    `bio` MEDIUMTEXT,\
-    `salt` BINARY(64) NOT NULL,\
-    PRIMARY KEY (`playerid`),\
-    UNIQUE INDEX `playerid_UNIQUE` (`playerid` ASC) VISIBLE,\
-    UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,\
-    UNIQUE INDEX `hash_UNIQUE` (`hash` ASC) VISIBLE,\
-    UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,\
-    UNIQUE INDEX `salt_UNIQUE` (`salt` ASC) VISIBLE);';
-    con.query(createUserTable, (err, result)=> {
-      if (err) throw err;
-    })
-    var createGamesTable =  `CREATE TABLE IF NOT EXISTS mafiaData.Games
+await con.execute(createSchema)
+var createUserTable = 'CREATE TABLE IF NOT EXISTS `mafiaData`.`userTable` (\
+  `playerid` INT NOT NULL AUTO_INCREMENT,\
+  `username` VARCHAR(45) NOT NULL,\
+  `hash` VARCHAR(45) NOT NULL,\
+  `email` VARCHAR(45) NOT NULL,\
+  `stats` JSON NULL,\
+  `friends` JSON NULL,\
+  `blocked` JSON NULL,\
+  `bio` MEDIUMTEXT,\
+  `salt` BINARY(64) NOT NULL,\
+  PRIMARY KEY (`playerid`),\
+  UNIQUE INDEX `playerid_UNIQUE` (`playerid` ASC) VISIBLE,\
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,\
+  UNIQUE INDEX `hash_UNIQUE` (`hash` ASC) VISIBLE,\
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,\
+  UNIQUE INDEX `salt_UNIQUE` (`salt` ASC) VISIBLE);';
+con.execute(createUserTable);
+var createGamesTable =  `CREATE TABLE IF NOT EXISTS mafiaData.Games
 (gameId int AUTO_INCREMENT PRIMARY KEY,
 port int,
 maxPlayers int,
@@ -36,31 +30,32 @@ lockedGame bool,
 startedGame bool,
 gameEnded bool
 )`
-con.query(createGamesTable, (err, result)=> {
-  if (err) throw err;
-  var createGamePlayersTable =  `CREATE TABLE IF NOT EXISTS mafiaData.gamePlayers
-(gameId int,
-uuid int,
-FOREIGN KEY (gameId) REFERENCES Games(gameId)
+con.execute(createGamesTable)
+var createGamePlayersTable =  `CREATE TABLE IF NOT EXISTS mafiaData.gamePlayers
+(gameId int unsigned,
+uuid int
 )`
-con.query(createGamePlayersTable, (err, result)=> {
-if (err) throw err;
-})
-
+con.execute(createGamePlayersTable)
 var createGameRolesTable = `
 CREATE TABLE IF NOT EXISTS mafiaData.gameRoles
 (gameId int,
-uuid  int unsigned,
-FOREIGN KEY (gameId) REFERENCES Games(gameId)
+roleConfig  int unsigned
 )`
-con.query(createGameRolesTable, (err, result)=> {
-if (err) throw err;
-})
-});
-})
+con.execute(createGameRolesTable)
+var createGameQueue = `
+CREATE TABLE IF NOT EXISTS mafiaData.gameTableQueue  (port INT)`
+con.execute(createGameQueue)
+var playerSocket = `CREATE TABLE IF NOT EXISTS mafiadata.playersocket
+(gameId int,
+websocketport int,
+uuid int unsigned,
+UNIQUE(uuid)
+)`
+con.execute(playerSocket)
 }
+
 async function getDatabase(){
-var con = mysql.createConnection({
+var con = await mysql.createConnection({
     host: "127.0.0.1",
 	port: "3306",
     user: "anthony",
--- server/server.js
+++ server/server.js
@@ -1,31 +1,58 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
-const db = require('./db/db');
-
-
+const database = require('./db/db');
 const userRouter = require('./routes/users');
-var cors = require('cors')
+const s = require('net').Socket();
 
+(async function(){
 app.use(cors())
 
 app.use(express.json());
-
-
+var db = await database.getDatabase();
 app.post('/users/register', (req, res)=>{
+
 
     const username = req.body.username;
     const hash = req.body.username;
     const email = req.body.username;
     const ip = req.body.username;
 
-    db.query('insert into usertable (username, hash, email, ip) values (?,?,?,?)',
+    db.execute('insert into usertable (username, hash, email, ip) values (?,?,?,?)',
     [username, hash , email, ip], (err, res) => {
         console.log(err)
     });
 
 })
 
+app.get('/joinGame', async (req,res)=>{
+  await db.query("LOCK TABLE mafiadata.gametablequeue WRITE");
+  var res = await db.query("SELECT PORT FROM mafiadata.gametablequeue LIMIT 1");
+  var port = res[0][0].PORT
+  s.connect(port, "127.0.0.1");
+  s.on('connect', ()=>{
+    var buffer = Buffer.alloc(256).fill('\0');
+    var game = {
+      cmd: 0,
+      roles:[0,1,0,0],
+      settings:0
+    }
+    buffer.write(JSON.stringify(game));
+    s.write(buffer, ()=>{
+      buffer = Buffer.alloc(256).fill('\0');
+      var cmd = {
+        cmd: 1,
+        playerid: 1
+      }
+      buffer.write(JSON.stringify(cmd));
+      s.write(buffer);
+    });
+});
+  await db.query("DELETE FROM mafiadata.gametablequeue WHERE port = ?",[port]);
+  await db.query("UNLOCK TABLES");
+})
+
 app.listen(3001, () =>{
     console.log("Server is listening on PORT: 3001")
 });
-
+})();
